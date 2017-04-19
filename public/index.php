8e732e309569a20b462ca96367ce6e662d3c3bb1
PXgamer <owzie123@gmail.com>
2017-04-19 23:37:08 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', realpath(__DIR__ . '/..') . DS);
+define('BASE_PATH', ROOT_PATH . 'public' . DS);
+define('SRC_PATH', ROOT_PATH . 'src' . DS);
+
+require(ROOT_PATH . 'vendor' . DS . 'autoload.php');
+
