f3b22c1bb9d925abd05c2c45a03556ef3898ec72
PXgamer <owzie123@gmail.com>
2017-04-20 11:38:53 +0100

    Updated the error and index routes

--- src/Controller/Main.php
+++ src/Controller/Main.php
@@ -6,9 +6,21 @@ class Main extends Controller
 {
     public function index()
     {
+        $this->smarty->display('index.tpl');
     }
 
-    public function error()
+    public function error($code = 404, $text = 'Page not found.', $data = false)
     {
+        $error = new \stdClass();
+        $error->code = $code;
+        $error->text = $text;
+        $error->data = $data;
+
+        $this->smarty->display(
+            'error.tpl',
+            [
+                'error' => $error
+            ]
+        );
     }
 }
