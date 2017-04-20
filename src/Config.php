fc22840ed0c832febea31e409b313e06e1681641
PXgamer <owzie123@gmail.com>
2017-04-20 12:17:09 +0100

    Added db config constants to the Config class

--- src/Config.php
+++ src/Config.php
@@ -4,5 +4,11 @@ namespace VS;
 
 class Config
 {
+    // App Configuration
     const APP_NAME = 'VS';
+
+    // Database Configuration
+    const DB_DSN = 'mysql:host=localhost;dbname=vs';
+    const DB_USER = 'root';
+    const DB_PASS = 'root';
 }
