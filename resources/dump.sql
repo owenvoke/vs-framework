d163c466d6ba91bdb965f15b80ec9defed430939
PXgamer <owzie123@gmail.com>
2017-04-20 17:17:34 +0100

    Updated SQL dump

--- resources/dump.sql
+++ resources/dump.sql
@@ -2,9 +2,43 @@ CREATE DATABASE IF NOT EXISTS vs;
 
 USE vs;
 
+# Create USERS table
 CREATE TABLE IF NOT EXISTS users (
   id       BIGINT PRIMARY KEY AUTO_INCREMENT UNIQUE,
   username VARCHAR(200) UNIQUE,
   email    VARCHAR(300) UNIQUE,
   password VARCHAR(500)
+);
+
+# Create USERS_INFO table
+CREATE TABLE IF NOT EXISTS users_info (
+  id     BIGINT PRIMARY KEY UNIQUE,
+  avatar VARCHAR(500)
+);
+
+# Create CATEGORIES table
+CREATE TABLE IF NOT EXISTS categories (
+  id   BIGINT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+  name VARCHAR(200) UNIQUE
+);
+
+# Create TAGS table
+CREATE TABLE IF NOT EXISTS tags (
+  id   BIGINT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+  name VARCHAR(150) UNIQUE
+);
+
+# Create VIDEOS table
+CREATE TABLE IF NOT EXISTS videos (
+  id          BIGINT PRIMARY KEY AUTO_INCREMENT UNIQUE,
+  hash        VARCHAR(40) UNIQUE,
+  title       VARCHAR(400),
+  description VARCHAR(1000),
+  category    BIGINT
+);
+
+# Create VIDEO_TAGS table
+CREATE TABLE IF NOT EXISTS video_tags (
+  id  BIGINT PRIMARY KEY,
+  tag BIGINT
 );
