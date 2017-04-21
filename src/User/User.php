ae1a44a8795f3bf04281e65358be610952d4ee6d
PXgamer <owzie123@gmail.com>
2017-04-21 14:28:58 +0100

    Added User/User class for getting user information

---
+++ src/User/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace VS\User;
+
+use VS\Config;
+
+class User
+{
+    public $id;
+    public $username;
+    public $email;
+    public $acl;
+    public $info;
+    public $videos;
+
+    public function __construct($username)
+    {
+        $db = Config::connect();
+        $stmt = $db->prepare('SELECT * FROM users WHERE username = :username');
+        $stmt->bindParam(':username', $username, \PDO::PARAM_STR);
+        $stmt->execute();
+        $result = $stmt->fetch(\PDO::FETCH_OBJ);
+        if ($result) {
+
+            foreach ($result as $key => $value) {
+                if (is_numeric($value)) {
+                    $value = (int)$value;
+                }
+                $this->$key = $value;
+            }
+
+            $stmt = $db->prepare('SELECT * FROM acls WHERE id = :id');
+            $stmt->bindParam(':id', $this->acl, \PDO::PARAM_STR);
+            $stmt->execute();
+            $this->acl = $stmt->fetch(\PDO::FETCH_OBJ);
+
+            $stmt = $db->prepare('SELECT * FROM videos WHERE uploader = :id');
+            $stmt->bindParam(':id', $this->id, \PDO::PARAM_STR);
+            $stmt->execute();
+            $this->videos = $stmt->fetchAll(\PDO::FETCH_OBJ);
+
+            $stmt = $db->prepare('SELECT * FROM users_info WHERE id = :id');
+            $stmt->bindParam(':id', $this->id, \PDO::PARAM_INT);
+            $stmt->execute();
+            $this->info = $stmt->fetch(\PDO::FETCH_OBJ);
+        }
+    }
+}
