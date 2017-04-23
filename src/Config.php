d61bf1261f5411b1978a07af83e6305b650b60a7
PXgamer <owzie123@gmail.com>
2017-04-23 22:18:48 +0100

    Fixed DSN DB Name

--- src/Config.php
+++ src/Config.php
@@ -11,7 +11,7 @@ class Config
     const APP_NAME = 'VS';
 
     // Database Configuration
-    const DB_DSN = 'mysql:host=localhost;dbname=VS';
+    const DB_DSN = 'mysql:host=localhost;dbname=vs';
     const DB_USER = 'root';
     const DB_PASS = 'root';
 
